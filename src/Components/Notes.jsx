cbbb3ac5111c727a5c376d17aefcfc4efc8e1950
Omkar Maurya <110991357+Omkar5001@users.noreply.github.com>
2023-07-24 13:43:23 +0530

    Initial commit

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> Write Here</h1>
+      <p>
+        Front-End Web Development ESSENTIALS | JavaScript and React | PROJECT
+        MAKING{" "}
+      </p>
+      <p>NOTE MAKING APP</p>
+    </div>
+  );
+}
+
+export default Notes;
